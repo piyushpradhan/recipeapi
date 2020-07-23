90f13300f537354935152790ce9b2aae562621b1
piyushpradhan <54734029+piyushpradhan@users.noreply.github.com>
2020-07-23 12:04:04 +0530

    fixed the create user error

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -12,20 +12,20 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('email', 'password', 'name')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
-        def create(self, validated_data):
-            """Create a new user with encrypted password and return it"""
-            return get_user_model().objects.create_user(**validated_data)
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it"""
+        return get_user_model().objects.create_user(**validated_data)
 
-        def update(self, instance, validated_data):
-            """Update the user credentials and set password properly"""
-            password = validated_data.pop('password', None)
-            user = super().update(instance, validated_data)
+    def update(self, instance, validated_data):
+        """Update the user credentials and set password properly"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
 
-            if password:
-                user.set_password(password)
-                user.save()
+        if password:
+            user.set_password(password)
+            user.save()
 
-            return user
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
